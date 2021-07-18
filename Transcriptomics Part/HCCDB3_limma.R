5a51a8b284690e71c3d933f8fa16c13e738133d9
basakbahcivanci <73958507+basakbahcivanci@users.noreply.github.com>
2021-07-18 12:29:23 +0100

    Update HCCDB3_limma.R
    Some comments added

--- Transcriptomics Part/HCCDB3_limma.R
+++ Transcriptomics Part/HCCDB3_limma.R
@@ -2,31 +2,33 @@ rm(list = ls())
 library(limma)
 library(EnhancedVolcano)
 
+# reading and storing data HCCDB3
 setwd('/Users/user/Documents/MSc BIOINFORMATICS/Individual Project/HCCDB3')
 data = read.csv('prepared_data3.csv', header = TRUE,row.names = 1)
 sample_info = read.csv('prepared_sample_info3.csv', header = TRUE, row.names = 1)
 
 
-
+# assigning type as a foctor for experimental design matrix
 ct <- factor(sample_info$TYPE)
-table(ct)
+
 # Create Design Matrix 433x2
 design <- model.matrix(~0+ct)
 # Need renaming the column
 colnames(design) <- levels(ct)
-
+# fitting linear model for each gene
 fit <- lmFit(data,design)
+# setting up comparisons of interest
 contrasts <- makeContrasts(Diff = HCC-Adjacent, levels=design)
+# to estimate original fit and the difference between groups
 fit2 <- contrasts.fit(fit, contrasts)
+# calculates moderated t-statistics
 fit3 <- eBayes(fit2, trend=TRUE)
-summary(decideTests(fit3, method="global"))
+# summary(decideTests(fit3, method="global"))
 
 deg = topTable(fit3, coef='Diff',number = nrow(data)) 
-dim(deg)[1]
-head(deg)
 
 
-quartz() # polygon edge not found error verdiginde
+# quartz() # for polygon edge not found error
 pdf(file = "HCCDB3 - Volcano.pdf")
 EnhancedVolcano(deg,
                 lab = rownames(deg),
@@ -48,5 +50,7 @@ write.csv(down,'HCCDB3_FC2_DEGS_DOWN.csv', quote = F,row.names = TRUE)
 up= deg[which(deg$logFC>=1),]
 write.csv(up,'HCCDB3_FC2_DEGS_UP.csv', quote = F,row.names = TRUE)
 
+# filtering DEGs from data and writing it out
 degs = data[rownames(data) %in% rownames(deg),]
 write.csv(degs,'DEGS_HCCDB3.csv', quote = F,row.names = TRUE)
+
