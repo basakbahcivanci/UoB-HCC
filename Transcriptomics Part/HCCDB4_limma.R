1795a3585c3c363120925535fdd7f84a9a376345
basakbahcivanci <73958507+basakbahcivanci@users.noreply.github.com>
2021-07-18 12:30:43 +0100

    Update HCCDB4_limma.R
    Some comments are added

--- Transcriptomics Part/HCCDB4_limma.R
+++ Transcriptomics Part/HCCDB4_limma.R
@@ -2,31 +2,31 @@ rm(list = ls())
 library(limma)
 library(EnhancedVolcano)
 
+# reading and storing data HCCDB4
 setwd('/Users/user/Documents/MSc BIOINFORMATICS/Individual Project/HCCDB4')
 data = read.csv('recent_data4.csv', header = TRUE,row.names = 1)
 sample_info = read.csv('recent_sample_info4.csv', header = TRUE, row.names = 1)
 
 
-
+# assigning type as a foctor for experimental design matrix
 ct <- factor(sample_info$TYPE)
-table(ct)
 # Create Design Matrix 
 design <- model.matrix(~0+ct)
 # Need renaming the column
 colnames(design) <- levels(ct)
-
+# fitting linear model for each gene
 fit <- lmFit(data,design)
-
+# setting up comparisons of interest
 contrasts <- makeContrasts(Diff = HCC-Adjacent, levels=design)
-
-
+# to estimate original fit and the difference between groups
 fit2 <- contrasts.fit(fit, contrasts)
+# calculates moderated t-statistics
 fit3 <- eBayes(fit2, trend=TRUE)
-summary(decideTests(fit3, method="global"))
+#summary(decideTests(fit3, method="global"))
+
 deg = topTable(fit3, coef='Diff',number = nrow(data))
 
-
-
+# quartz() # for polygon edge not found error
 pdf(file = "HCCDB4 - Volcano.pdf")
 EnhancedVolcano(deg,
                 lab = rownames(deg),
@@ -48,5 +48,6 @@ write.csv(down,'HCCDB4_FC2_DEGS_DOWN.csv', quote = F,row.names = TRUE)
 up= deg[which(deg$logFC>=1),]
 write.csv(up,'HCCDB4_FC2_DEGS_UP.csv', quote = F,row.names = TRUE)
 
+# filtering DEGs from data and writing it out
 degs = data[rownames(data) %in% rownames(deg),]
 write.csv(degs,'DEGS_HCCDB4.csv', quote = F,row.names = TRUE)
